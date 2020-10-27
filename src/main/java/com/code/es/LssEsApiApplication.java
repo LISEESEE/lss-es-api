a86c6a2a03a1c44343e9fe46f40b385655fa7488
lisensen <641307158@qq.com>
2020-10-27 21:21:14 +0800

    add all

---
+++ src/main/java/com/code/es/LssEsApiApplication.java
@@ -0,0 +1,13 @@
+package com.code.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LssEsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LssEsApiApplication.class, args);
+    }
+
+}
